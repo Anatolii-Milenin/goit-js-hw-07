5e03a7ea2f3cd048719ba4c51d394017e7e75118
Anatolii Milenin <anatolymv@gmail.com>
2023-02-19 11:25:31 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,54 +1,25 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-// const renderList = () =>
-//   galleryItems
-//     .map(
-//       ({ preview, original, description }) =>
-//         `<div><a class="gallery__link" href="${original}">   <img
-//       class="gallery__image"
-//       src="${preview}"
-//       data-source="${original}"
-//       alt="${description}"
-//     /></a></div>`
-//     )
-//     .join("");
+const renderList = () =>
+  galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div><a class="gallery__link" href="${original}">   <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    /></a></div>`
+    )
+    .join("");
 
-// const insertListItem = (string) => {
-//   const galleryItems = document.querySelector(".gallery");
-//   galleryItems.insertAdjacentHTML("beforeend", string);
-// };
+const insertListItem = (string) => {
+  const galleryItems = document.querySelector(".gallery");
+  galleryItems.insertAdjacentHTML("beforeend", string);
+};
 
-// insertListItem(renderList(galleryItems));
-
-const gallery = document.querySelector(".gallery");
-
-const items = [];
-
-galleryItems.forEach((element) => {
-  const galleryItem = document.createElement("div");
-  galleryItem.classList.add("gallery__item");
-
-  const galleryLink = document.createElement("a");
-  galleryLink.classList.add("gallery__link");
-
-  galleryLink.href = element.original;
-
-  const galleryImg = document.createElement("img");
-  galleryImg.classList.add("gallery__image");
-
-  galleryImg.src = element.preview;
-
-  galleryImg.setAttribute("data-source", element.original);
-
-  galleryImg.alt = element.description;
-
-  galleryItem.append(galleryLink);
-  galleryLink.append(galleryImg);
-  items.push(galleryItem);
-});
-
-gallery.append(...items);
+insertListItem(renderList(galleryItems));
 
 document.addEventListener("click", (e) => {
   e.preventDefault();
@@ -58,11 +29,8 @@ document.addEventListener("click", (e) => {
   }
 
   const imgSelected = e.target.getAttribute("data-source");
-
   const template = basicLightbox.create(
-    `
-    <img src="${imgSelected}" width="800" height="600">
-    `,
+    `<img src="${imgSelected}" width="800" height="600">`,
 
     {
       onShow: () => {
