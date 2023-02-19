b9c1d909289312c132463d2e507eb9925d293222
Anatolii Milenin <anatolymv@gmail.com>
2023-02-19 11:47:48 +0200

    hw-07-js-task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const renderList = () =>
+  galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div><a class="gallery__link" href="${original}">   <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    /></a></div>`
+    )
+    .join("");
+
+const insertListItem = (string) => {
+  const galleryItems = document.querySelector(".gallery");
+  galleryItems.insertAdjacentHTML("beforeend", string);
+};
+
+insertListItem(renderList(galleryItems));
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
